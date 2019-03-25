108c539a2887b64c9666c6813b62d7e54cb5f038
Mohammad-Rahmdel <unknown13766@yahoo.com>
2019-03-25 18:09:39 +0430

    almost done

--- Neural Networks and Deep Learning/week2/week2_PA1.py
+++ Neural Networks and Deep Learning/week2/week2_PA1.py
@@ -90,8 +90,71 @@ def propagate(w, b, X, Y):
     return cost, dw, db
 
 
+# w, b, X, Y = np.array([[1], [2]]), 2, np.array([[1,2], [3,4]]), np.array([[1, 0]])
+# cost, dw, db = propagate(w, b, X, Y)
+# print ("dw = " + str(dw))
+# print ("db = " + str(db))
+# print ("cost = " + str(cost))
+
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    """
+    This function optimizes w and b by running a gradient descent algorithm
+    
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of shape (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat), of shape (1, number of examples)
+    num_iterations -- number of iterations of the optimization loop
+    learning_rate -- learning rate of the gradient descent update rule
+    print_cost -- True to print the loss every 100 steps
+    
+    Returns:
+    costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
+    
+    Tips:
+    You basically need to write down two steps and iterate through them:
+        1) Calculate the cost and the gradient for the current parameters. Use propagate().
+        2) Update the parameters using gradient descent rule for w and b.
+    """
+    costs = []
+    for _ in range(num_iterations):
+        cost, dw, db = propagate(w, b, X, Y)
+        costs.append(cost)
+        # w -= learning_rate * dw
+        # b -= learning_rate * db
+        w = w - learning_rate * dw  
+        b = b - learning_rate * db
+    
+    return costs, w, b, dw, db
+
+
 w, b, X, Y = np.array([[1], [2]]), 2, np.array([[1,2], [3,4]]), np.array([[1, 0]])
-cost, dw, db = propagate(w, b, X, Y)
-print ("dw = " + str(dw))
-print ("db = " + str(db))
-print ("cost = " + str(cost))
+costs, w, b, dw, db = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
+# print ("w = " + str(w))
+# print ("b = " + str(b))
+# print ("dw = " + str(dw))
+# print ("db = " + str(db))
+
+
+def predict(w, b, X):
+    '''
+    Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
+    
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    
+    Returns:
+    Y_prediction -- a numpy array (vector) containing all predictions (0/1) for the examples in X
+    '''
+
+    Y_hat = sigmoid(np.dot(w.T,X) + b)
+    Y_prediction = np.round(Y_hat) #0 (if activation <= 0.5) or 1 (if activation > 0.5)
+    return Y_prediction
+
+print("predictions = " + str(predict(w, b, X)))
+
+
