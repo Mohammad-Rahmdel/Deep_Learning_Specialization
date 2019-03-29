62e331bf089e7c16d11dbf21d5ccece3476aff0a
Mohammad-Rahmdel <unknown13766@yahoo.com>
2019-03-30 00:30:11 +0430

    PA4 in progress ...

--- Neural Networks and Deep Learning/week4/PA4/PA4.py
+++ Neural Networks and Deep Learning/week4/PA4/PA4.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import scipy
 from PIL import Image
 from scipy import ndimage
-from dnn_app_utils import *
+from dnn_app_utils_v2 import *
 
 
 plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
@@ -22,3 +22,20 @@ plt.rcParams['image.cmap'] = 'gray'
 
 
 np.random.seed(1)
+
+
+
+train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+
+# # Example of a picture
+# index = 10
+# plt.imshow(train_x_orig[index])
+# print ("y = " + str(train_y[0,index]) + ". It's a " + classes[train_y[0,index]].decode("utf-8") +  " picture.")
+# plt.show()
+
+
+
+train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T   
+test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+train_x = train_x_flatten/255.
+test_x = test_x_flatten/255.
