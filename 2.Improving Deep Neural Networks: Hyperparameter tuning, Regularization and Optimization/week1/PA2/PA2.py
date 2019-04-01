d0fbfff8534c2a70703cc2020ce2e9e797c99fe0
Mohammad-Rahmdel <unknown13766@yahoo.com>
2019-04-01 23:56:11 +0430

    Programming Assignment 2 Successfully Completed ~_~

--- 2.Improving Deep Neural Networks: Hyperparameter tuning, Regularization and Optimization/week1/PA2/PA2.py
+++ 2.Improving Deep Neural Networks: Hyperparameter tuning, Regularization and Optimization/week1/PA2/PA2.py
@@ -19,3 +19,272 @@ plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
 train_X, train_Y, test_X, test_Y = load_2D_dataset()
+plt.show()
+
+
+
+def model(X, Y, learning_rate = 0.3, num_iterations = 30000, print_cost = True, lambd = 0, keep_prob = 1):
+    """
+    Implements a three-layer neural network: LINEAR->RELU->LINEAR->RELU->LINEAR->SIGMOID.
+    
+    Arguments:
+    X -- input data, of shape (input size, number of examples)
+    Y -- true "label" vector (1 for blue dot / 0 for red dot), of shape (output size, number of examples)
+    learning_rate -- learning rate of the optimization
+    num_iterations -- number of iterations of the optimization loop
+    print_cost -- If True, print the cost every 10000 iterations
+    lambd -- regularization hyperparameter, scalar
+    keep_prob - probability of keeping a neuron active during drop-out, scalar.
+    
+    Returns:
+    parameters -- parameters learned by the model. They can then be used to predict.
+    """
+        
+    grads = {}
+    costs = []                            # to keep track of the cost
+    m = X.shape[1]                        # number of examples
+    layers_dims = [X.shape[0], 20, 3, 1]
+    
+    # Initialize parameters dictionary.
+    parameters = initialize_parameters(layers_dims)
+
+    # Loop (gradient descent)
+
+    for i in range(0, num_iterations):
+
+        # Forward propagation: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID.
+        if keep_prob == 1:
+            a3, cache = forward_propagation(X, parameters)
+        elif keep_prob < 1:
+            a3, cache = forward_propagation_with_dropout(X, parameters, keep_prob)
+        
+        # Cost function
+        if lambd == 0:
+            cost = compute_cost(a3, Y)
+        else:
+            cost = compute_cost_with_regularization(a3, Y, parameters, lambd)
+            
+        # Backward propagation.
+        assert(lambd == 0 or keep_prob == 1)    # it is possible to use both L2 regularization and dropout, 
+                                            # but this assignment will only explore one at a time
+        if lambd == 0 and keep_prob == 1:
+            grads = backward_propagation(X, Y, cache)
+        elif lambd != 0:
+            grads = backward_propagation_with_regularization(X, Y, cache, lambd)
+        elif keep_prob < 1:
+            grads = backward_propagation_with_dropout(X, Y, cache, keep_prob)
+        
+        # Update parameters.
+        parameters = update_parameters(parameters, grads, learning_rate)
+        
+        # Print the loss every 10000 iterations
+        if print_cost and i % 10000 == 0:
+            print("Cost after iteration {}: {}".format(i, cost))
+        if print_cost and i % 1000 == 0:
+            costs.append(cost)
+    
+    # plot the cost
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (x1,000)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+    
+    return parameters
+
+
+# parameters = model(train_X, train_Y)
+# print("On the training set:")
+# predictions_train = predict(train_X, train_Y, parameters)
+# print("On the test set:")
+# predictions_test = predict(test_X, test_Y, parameters)
+
+# plt.title("Model without regularization")
+# axes = plt.gca()
+# axes.set_xlim([-0.75, 0.40])
+# axes.set_ylim([-0.75, 0.65])
+# plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+
+
+""" L2 Regularization """
+
+def compute_cost_with_regularization(A3, Y, parameters, lambd):
+    """
+    Implement the cost function with L2 regularization. 
+    
+    Arguments:
+    A3 -- post-activation, output of forward propagation, of shape (output size, number of examples)
+    Y -- "true" labels vector, of shape (output size, number of examples)
+    parameters -- python dictionary containing parameters of the model
+    
+    Returns:
+    cost - value of the regularized loss function 
+    """
+    m = Y.shape[1]
+    cost = compute_cost(A3, Y) # without Regularization term
+    L = len(parameters) // 2
+    W = 0
+    for i in range(L):
+        W += np.sum(parameters["W" + str(i+1)] ** 2)
+    
+    cost += (lambd * W) / (2 * m)
+    
+    return cost
+
+
+def reluDerivative(x):
+    x[x<=0] = 0
+    x[x>0] = 1
+    return x
+
+def backward_propagation_with_regularization(X, Y, cache, lambd):
+    """
+    Implements the backward propagation of our baseline model to which we added an L2 regularization.
+    
+    Arguments:
+    X -- input dataset, of shape (input size, number of examples)
+    Y -- "true" labels vector, of shape (output size, number of examples)
+    cache -- cache output from forward_propagation()
+    lambd -- regularization hyperparameter, scalar
+    
+    Returns:
+    gradients -- A dictionary with the gradients with respect to each parameter, activation and pre-activation variables
+    """
+
+    m = X.shape[1]
+
+    (Z1, A1, W1, _, Z2, A2, W2, _, _, A3, W3, _) = cache
+    
+    dZ3 = A3 - Y
+    dW3 = 1 / m * np.dot(dZ3, A2.T) + (lambd * W3) / m
+    db3 = 1 / m * np.sum(dZ3, axis=1, keepdims=True)
+    
+    dA2 = np.dot(W3.T, dZ3)
+    dZ2 = dA2 * reluDerivative(Z2)
+    dW2 = 1 / m * np.dot(dZ2, A1.T) + (lambd * W2) / m
+    db2 = 1 / m * np.sum(dZ2, axis=1, keepdims=True)
+    
+    dA1 = np.dot(W2.T, dZ2)
+    dZ1 = dA1 * reluDerivative(Z1)
+    dW1 = 1 / m * np.dot(dZ1, X.T) + (lambd * W1) / m
+    db1 = 1 / m * np.sum(dZ1, axis=1, keepdims=True)
+    
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3, "dA2": dA2,
+                 "dZ2": dZ2, "dW2": dW2, "db2": db2, "dA1": dA1, 
+                 "dZ1": dZ1, "dW1": dW1, "db1": db1}
+    
+    return gradients
+
+
+# parameters = model(train_X, train_Y, lambd=0.7)
+# print("On the train set:")
+# predictions_train = predict(train_X, train_Y, parameters)
+# print("On the test set:")
+# predictions_test = predict(test_X, test_Y, parameters)
+
+# plt.title("Model with L2-regularization")
+# axes = plt.gca()
+# axes.set_xlim([-0.75,0.40])
+# axes.set_ylim([-0.75,0.65])
+# plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+
+
+
+""" Dropout """
+ 
+def forward_propagation_with_dropout(X, parameters, keep_prob=0.5):
+    """
+    Implements the forward propagation: LINEAR -> RELU + DROPOUT -> LINEAR -> RELU + DROPOUT -> LINEAR -> SIGMOID.
+    
+    Arguments:
+    X -- input dataset, of shape (2, number of examples)
+    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3":
+    keep_prob - probability of keeping a neuron active during drop-out, scalar
+    
+    Returns:
+    A3 -- last activation value, output of the forward propagation, of shape (1,1)
+    cache -- tuple, information stored for computing the backward propagation
+    """
+    
+    np.random.seed(1)
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    W3 = parameters["W3"]
+    b3 = parameters["b3"]
+
+    Z1 = np.dot(W1, X) + b1
+    A1 = relu(Z1)
+    D1 = np.random.rand(A1.shape[0], A1.shape[1]) < keep_prob       
+    A1 = A1 * D1
+    A1 /= keep_prob
+
+    Z2 = np.dot(W2, A1) + b2
+    A2 = relu(Z2)
+    D2 = np.random.rand(A2.shape[0], A2.shape[1]) < keep_prob         
+    A2 = A2 * D2
+    A2 /= keep_prob
+    
+    Z3 = np.dot(W3, A2) + b3
+    A3 = sigmoid(Z3) 
+
+    cache = (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3)
+    return A3, cache
+
+
+
+def backward_propagation_with_dropout(X, Y, cache, keep_prob):
+    """
+    Implements the backward propagation of our baseline model to which we added dropout.
+    
+    Arguments:
+    X -- input dataset, of shape (2, number of examples)
+    Y -- "true" labels vector, of shape (output size, number of examples)
+    cache -- cache output from forward_propagation_with_dropout()
+    keep_prob - probability of keeping a neuron active during drop-out, scalar
+    
+    Returns:
+    gradients -- A dictionary with the gradients with respect to each parameter, activation and pre-activation variables
+    """
+    
+    m = X.shape[1]
+    (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3) = cache
+    
+    dZ3 = A3 - Y
+    dW3 = 1 / m * np.dot(dZ3, A2.T)
+    db3 = 1 / m * np.sum(dZ3, axis=1, keepdims=True)
+    
+    dA2 = np.dot(W3.T, dZ3)
+    dA2 = dA2 * D2            
+    dA2 = dA2 / keep_prob             
+    dZ2 = dA2 * reluDerivative(Z2)
+    dW2 = 1. / m * np.dot(dZ2, A1.T)
+    db2 = 1. / m * np.sum(dZ2, axis=1, keepdims=True)
+    
+    dA1 = np.dot(W2.T, dZ2)
+    dA1 = dA1 * D1              
+    dA1 = dA1 / keep_prob           
+    dZ1 = dA1 * reluDerivative(Z1)
+    dW1 = 1 / m * np.dot(dZ1, X.T)
+    db1 = 1 / m * np.sum(dZ1, axis=1, keepdims=True)
+    
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,"dA2": dA2,
+                 "dZ2": dZ2, "dW2": dW2, "db2": db2, "dA1": dA1, 
+                 "dZ1": dZ1, "dW1": dW1, "db1": db1}
+    
+    return gradients
+
+
+"""
+**Model**                              **Train accuracy**    **test accuracy** 
+3-layer NN without regularization           95%                  	91.5% 
+3-layer NN with L2-regularization           93%                 	93% 
+3-layer NN with dropout                     94%                  	95%
+
+-Regularization will help you reduce overfitting.
+-Regularization will drive your weights to lower values.
+-L2 regularization and Dropout are two very effective regularization techniques.
+"""
