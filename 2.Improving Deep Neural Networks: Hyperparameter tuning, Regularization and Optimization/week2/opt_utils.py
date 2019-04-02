75b29e00c6a6927a4768bb7c3cc365f04da3a99b
Mohammad-Rahmdel <unknown13766@yahoo.com>
2019-04-02 15:05:30 +0430

    error fixed

--- 2.Improving Deep Neural Networks: Hyperparameter tuning, Regularization and Optimization/week2/opt_utils.py
+++ 2.Improving Deep Neural Networks: Hyperparameter tuning, Regularization and Optimization/week2/opt_utils.py
@@ -68,10 +68,7 @@ def initialize_parameters(layer_dims):
     for l in range(1, L):
         parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1])*  np.sqrt(2 / layer_dims[l-1])
         parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
-        
-        assert(parameters['W' + str(l)].shape == layer_dims[l], layer_dims[l-1])
-        assert(parameters['W' + str(l)].shape == layer_dims[l], 1)
-        
+       
     return parameters
 
 
